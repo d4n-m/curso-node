133b08a9eb65b1c1710895bcec6c9b69dce184db
d4n-m <dumy.tm@gmail.com>
2021-09-17 11:40:59 +0200

    .

---
+++ index.js
@@ -0,0 +1,14 @@
+//const miArray = require()
+
+import chalk from "chalk";
+import os from "os";
+
+let mivar = 'hola';
+                      
+export let miArray = ['hola', 'mundo'];
+
+const otroArray = ['martes', 'pluto'];
+
+console.log(chalk.redBright(os.platform()));
+
+export default miArray;
